23f9a5ca1703e962d0f9a404e6379fc38109720c
kispetiocseny <kispeter9315@gmail.com>
2023-06-14 17:23:56 +0200

    "Nyilak és a cookik telefon nézetben"

--- script.js
+++ script.js
@@ -51,3 +51,61 @@ form.addEventListener('submit', (event) => {
   }
   alert("Sikeres üzenet küldés!")
 });
+
+
+// Ellenőrizzük, hogy a cookie-t már elfogadták-e
+if (!getCookie('cookie_accepted')) {
+  // Ha még nem fogadták el a cookie-t, megjelenítjük a figyelmeztetést
+  document.getElementById('cookie-banner').style.display = 'block';
+}
+
+// Elfogadás gomb eseménykezelője
+document.getElementById('cookie-accept-btn').addEventListener('click', function() {
+  // Beállítjuk a cookie-t
+  setCookie('cookie_accepted', true, 365);
+
+  // Elrejtjük a figyelmeztetést
+  document.getElementById('cookie-banner').style.display = 'none';
+});
+
+// Cookie beállítása
+function setCookie(name, value, days) {
+  var expires = '';
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; path=/';
+}
+
+// Cookie lekérdezése
+function getCookie(name) {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+          c = c.substring(1, c.length);
+      }
+      if (c.indexOf(nameEQ) === 0) {
+          return c.substring(nameEQ.length, c.length);
+      }
+  }
+  return null;
+}
+
+
+//Összehúzásra a nyil menjen a szöveggel.
+function setArrowPosition() {
+  const arrowUp = document.querySelector('.resume-content .arrow-up');
+  const arrowDown = document.querySelector('.resume-content .arrow-down');
+  const contentWidth = document.querySelector('.resume-content').offsetWidth;
+  const arrowsWidth = arrowUp.offsetWidth;
+
+  arrowUp.style.left = `${(contentWidth - arrowsWidth) / 2}px`;
+  arrowDown.style.left = `${(contentWidth - arrowsWidth) / 2}px`;
+}
+
+window.addEventListener('resize', setArrowPosition);
+setArrowPosition();
